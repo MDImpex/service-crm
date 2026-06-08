eb114ea5fe9c859ecbfc68d16ad982c81b8ea5c6
Valdas MD Impex <valdasjanciauskas@gmail.com>
2026-06-08 14:52:46 +0300

    Sutvarkyta modalo struktūra ir komentarų sistema

--- src/App.jsx
+++ src/App.jsx
@@ -173,7 +173,7 @@ const handleAddComment = async (text) => {
   });
 
   const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVudWNydHJqYW9ha2FjaHNydWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMzA5NjgsImV4cCI6MjA5MzcwNjk2OH0.srfXrYR5MCzUMBwV-mm7mkiepg2ATOW2WsG8ldm920k'
-  const BASE_URL = 'https://enucrtrjaoakachsrubi.supabase.co/rest/v1/equipment'
+  const BASE_URL = 'https://enucrtrjaoakachsrubi.supabase.co/rest/v1';
 
   useEffect(() => { localStorage.setItem('crm_columns', JSON.stringify(columns)) }, [columns])
   useEffect(() => { localStorage.setItem('crm_widths', JSON.stringify(widths)) }, [widths])
